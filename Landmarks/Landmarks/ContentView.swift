a92b3bb55a0ce60452d173cc64404d61ce83c526
비긴어브랜드 <tripcoach@bigin-eobeulaendeuui-Macmini.local>
2024-01-17 23:59:39 +0900

    Added my project

---
+++ Landmarks/Landmarks/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by me on 2024/01/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello SwiftUI!!")
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,19 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 비긴어브랜드 on 2024/01/17.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    //@main은 entry point : App프로토콜로 구성됨
+    var body: some Scene {
+        //App의 body요소는 하나 이상의 Scene를 반환한다.
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
