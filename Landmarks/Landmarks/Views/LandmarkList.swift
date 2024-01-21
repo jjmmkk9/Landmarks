1f30db6f73acd4e2e50bcf66086bee58c7c1ce74
jjmmkk9 <abcdqwerty0192@gmail.com>
2024-01-21 15:28:13 +0900

    토글로 사용자가 필터할 수 있게 구현

--- Landmarks/Landmarks/Views/LandmarkList.swift
+++ Landmarks/Landmarks/Views/LandmarkList.swift
@@ -20,15 +20,23 @@ struct LandmarkList: View {
             //Key Path 문법 : landmarks 배열의 각 요소에서 id 프로퍼티를 추출
             //List(landmarks, id: \.id)
             //Landmark에 Identifiable 프로토콜 달아줬기 때문에 이제 저 id 파라미터는 필요 없어!!!
-            List(filteredLandmarks) { landmark in
-                            NavigationLink {
-                                LandmarkDetail(landmark: landmark)
-                                //label은 navigationLink에 표시될 내용
-                            } label: {
-                                LandmarkRow(landmark: landmark)
-                            }
+            List {          //$을 사용해야 Binding<Bool>으로 넘길수 있음
+                Toggle(isOn: $showFavoritesOnly){
+                    Text("Favorites only")
+                }
+                ForEach(filteredLandmarks) {landmark in
+                    NavigationLink {
+                        LandmarkDetail(landmark: landmark)
+                    //label은 navigationLink에 표시될 내용
+                    } label: {
+                        LandmarkRow(landmark: landmark)
                         }
+                    }
+                }
+            .animation(.default, value: filteredLandmarks)
             .navigationTitle("Landmarks")
+            
+            //detail은 아이폰에서는 안보이고 ipad에서 보이나봄
         } detail: {
             Text("Select a Landmark")
         }
