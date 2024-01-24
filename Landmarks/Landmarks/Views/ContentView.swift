5cf73b395ad604d43706ea49e320715b01bf062e
jjmmkk9 <abcdqwerty0192@gmail.com>
2024-01-24 16:27:28 +0900

    contentView 에서 탭 구현(어렵당)

--- Landmarks/Landmarks/Views/ContentView.swift
+++ Landmarks/Landmarks/Views/ContentView.swift
@@ -8,8 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selection: Tab = .featured
+    
+    enum Tab{
+        case featured
+        case list
+    }
     var body: some View {
-        LandmarkList()
+        TabView(selection: $selection) {
+//            Text("Tab Content 1").tabItem { /*@START_MENU_TOKEN@*/Text("Tab Label 1")/*@END_MENU_TOKEN@*/ }.tag(1)
+//            Text("Tab Content 2").tabItem { /*@START_MENU_TOKEN@*/Text("Tab Label 2")/*@END_MENU_TOKEN@*/ }.tag(2)
+            CategoryHome()
+                .tabItem { Label("Featured", systemImage: "star") }
+                .tag(Tab.featured)
+            
+            LandmarkList()
+                .tabItem { Label("List", systemImage: "list.bullet") }
+                .tag(Tab.list)
+            
+        }
     }
 }
 
