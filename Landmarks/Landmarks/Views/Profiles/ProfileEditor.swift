86a30cb991763f99082790a99e73129e55b85af4
jjmmkk9 <abcdqwerty0192@gmail.com>
2024-01-25 12:14:51 +0900

    프로필 수정뷰의 캔슬버튼 달고 변경 반영

--- Landmarks/Landmarks/Views/Profiles/ProfileEditor.swift
+++ Landmarks/Landmarks/Views/Profiles/ProfileEditor.swift
@@ -10,6 +10,13 @@ import SwiftUI
 struct ProfileEditor: View {
     @Binding var profile : Profile
     
+    var dateRange : ClosedRange<Date> {
+        let min = Calendar.current.date(byAdding: .year, value: -1, to: profile.goalDate)!
+        let max = Calendar.current.date(byAdding: .year, value: 1, to: profile.goalDate)!
+        return min...max
+    }
+    
+    
     var body: some View {
         List{
             HStack {
@@ -21,6 +28,16 @@ struct ProfileEditor: View {
             }
             Toggle(isOn: $profile.prefersNotifications){
                 Text("Enable Notifications")
+            }
+                    //Picker selection 요소는 기본 값 {다른 값은 클로저 안에 뿌리기
+            Picker("Seasonal Photo", selection: $profile.seasonalPhoto){
+                ForEach(Profile.Season.allCases){ season in
+                    Text(season.rawValue).tag(season)
+                }
+            }
+                //in프로퍼티에 ClosedRange<Date>주면 특정 기간 안에서 선택하게
+            DatePicker(selection: $profile.goalDate, in: dateRange , displayedComponents: .date){
+                Text("Goal date")
             }
         }
     }
--- Landmarks/Landmarks/Views/Profiles/ProfileHost.swift
+++ Landmarks/Landmarks/Views/Profiles/ProfileHost.swift
@@ -8,19 +8,33 @@
 import SwiftUI
 
 struct ProfileHost: View {
-    @Environment(\.editMode) var editMode // editMode 가 뭐죵
+    @Environment(\.editMode) var editMode // editMode로 edit중인지 아니니지 확인
+    
     @Environment(ModelData.self) var modelData
     @State private var draftProfile = Profile.default
     var body: some View {
         VStack (alignment: .leading, spacing: 20){
             HStack{
+                if editMode?.wrappedValue == .active {
+                        //디자인 차이가 없어도 role이 .cancel 이면 실제 프로필 데이터에 편집 내용을 적용하지 않는다.
+                    Button("Cancel", role: .cancel) {
+                        draftProfile = modelData.profile
+                        editMode?.animation().wrappedValue = .inactive
+                    }
+                }
                 Spacer()
                 EditButton()
             }
-            if editMode?.wrappedValue == .inactive{ //editMode가 비활성화면 프로필요약 보여줌
+            if editMode?.wrappedValue == .inactive{ //editMode가 비활성화면 ProfileSummary 보여줌
                 ProfileSummary(profile: modelData.profile)
             }else{
                 ProfileEditor(profile: $draftProfile)
+                    .onAppear{ // draftProfile에 실제 디폴트 프로필 주입(복사)
+                        draftProfile = modelData.profile
+                    }
+                    .onDisappear{ //모델 디폴트 프로필에 복사본 반영
+                        modelData.profile = draftProfile
+                    }
             }
         }
         .padding()
